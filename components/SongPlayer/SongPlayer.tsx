8243ec174ec3655bd9c0156c39695639488f125b
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2022-08-10 16:50:38 -0700

    :construction: def not this - not perf

--- components/SongPlayer/SongPlayer.tsx
+++ components/SongPlayer/SongPlayer.tsx
@@ -1,4 +1,5 @@
 import { Track } from '@tonejs/midi';
+import { Note } from '@tonejs/midi/dist/Note';
 import React, { useEffect, useRef, useState } from 'react'
 import * as Tone from 'tone'
 
@@ -8,15 +9,39 @@ type Props = {
 }
 
 const SongPlayer = ({playing, track}: Props) => {
+    const [notes, setNotes] = useState<Note[]>([])
     const [currentTime, setCurrentTime] = useState(new Date())
     const initialTime = useRef<Date | null>(null)
     console.log('track', track)
     // Did we hit last note? Stop! 
     const lastNote = track.notes[track.notes.length - 1];
+    // Current seconds of song
     const songDuration = initialTime.current ? currentTime - initialTime.current : 0;
+    // Our graph goes from 0 - FRAME_HEIGHT_TIME seconds
+    const FRAME_HEIGHT_TIME = 10;
 
 
+    // Note logic
+    useEffect(() => {
+        let newNotes: Note[] = [];
+      track.notes.forEach((note) => {
+        if(note.time <= songDuration + FRAME_HEIGHT_TIME) {
+            newNotes = [
+                ...newNotes,
+                note,
+            ]
+        }
+      })
 
+    if(newNotes.length > 0) setNotes((prevNotes) => ([
+        ...prevNotes.filter((note) => note.time + note.duration < songDuration),
+        ...newNotes
+    ]))
+    
+    }, [track.notes, songDuration])
+    
+
+    // Play/pause logic
   useEffect(() => {
     //@ts-ignore
     let interval;
@@ -55,6 +80,10 @@ const SongPlayer = ({playing, track}: Props) => {
         <h1>Current Time: {currentTime.toTimeString()}</h1>
         <h1>Initial Time: {startTime}</h1>
         <h1>Song Duration: {songDuration}</h1>
+
+        <div>
+            {notes.map(note => <h2 key={note.name}>{note.name} - {note.duration}</h2>)}
+        </div>
     </div>
   )
 }
