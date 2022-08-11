866ff7f1313e772fa7b289eb924e7750f973bacb
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2022-08-10 17:05:29 -0700

    :zap: better

--- components/SongPlayer/SongPlayer.tsx
+++ components/SongPlayer/SongPlayer.tsx
@@ -16,30 +16,10 @@ const SongPlayer = ({playing, track}: Props) => {
     // Did we hit last note? Stop! 
     const lastNote = track.notes[track.notes.length - 1];
     // Current seconds of song
-    const songDuration = initialTime.current ? currentTime - initialTime.current : 0;
+    const songDuration = initialTime.current ? (currentTime - initialTime.current) / 100 : 0;
     // Our graph goes from 0 - FRAME_HEIGHT_TIME seconds
     const FRAME_HEIGHT_TIME = 10;
 
-
-    // Note logic
-    useEffect(() => {
-        let newNotes: Note[] = [];
-      track.notes.forEach((note) => {
-        if(note.time <= songDuration + FRAME_HEIGHT_TIME) {
-            newNotes = [
-                ...newNotes,
-                note,
-            ]
-        }
-      })
-
-    if(newNotes.length > 0) setNotes((prevNotes) => ([
-        ...prevNotes.filter((note) => note.time + note.duration < songDuration),
-        ...newNotes
-    ]))
-    
-    }, [track.notes, songDuration])
-    
 
     // Play/pause logic
   useEffect(() => {
@@ -75,6 +55,9 @@ const SongPlayer = ({playing, track}: Props) => {
   const startTime = initialTime.current ? initialTime.current.toTimeString() : 0;
     console.log('time', songDuration, songDuration / 100)
 
+    const visibleNotes = track.notes.filter((note) => note.time + note.duration >= songDuration && note.time + note.duration <= songDuration + FRAME_HEIGHT_TIME);
+  console.log('visible notes', visibleNotes)
+
   return (
     <div>
         <h1>Current Time: {currentTime.toTimeString()}</h1>
@@ -82,7 +65,7 @@ const SongPlayer = ({playing, track}: Props) => {
         <h1>Song Duration: {songDuration}</h1>
 
         <div>
-            {notes.map(note => <h2 key={note.name}>{note.name} - {note.duration}</h2>)}
+            {visibleNotes.map(note => <h2 key={`${note.name}-${note.time}`}>{note.name} - {note.time} - {note.duration}</h2>)}
         </div>
     </div>
   )
